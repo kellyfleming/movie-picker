778dc953cdea6c950344da2b2a23fe58f122f3ce
Kelly Fleming <kflemin3@ncsu.edu>
2020-06-12 12:42:38 -0400

    skeleton App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {Switch, Route} from "react-router-dom";
 import filmData from "./data";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+        {/* Dropdown film picker goes here */}
+        <Switch>
+            <Route>
+                {/* Film component */}
+            </Route>
+        </Switch>
     </div>
   );
 }
