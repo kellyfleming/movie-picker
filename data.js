4e56985106e7f347fd93b54d7e1322026655ff88
Kelly Fleming <kflemin3@ncsu.edu>
2020-06-12 12:11:18 -0400

    add film data

---
+++ data.js
@@ -0,0 +1,142 @@
+import witchPoster from "./";
+import witchHeader from "./";
+
+import littleWomenPoster from "./";
+import littleWomenHeader from "./";
+
+import ladyBirdPoster from "./";
+import ladyBirdHeader from "./";
+
+import knivesOutPoster from "./";
+import knivesOutHeader from "./";
+
+import booksmartPoster from "./";
+import booksmartHeader from "./";
+
+import columbusPoster from "./";
+import columbusHeader from "./";
+
+import arrivalPoster from "./";
+import arrivalHeader from "./";
+
+import starWarsPoster from "./";
+import starWarsHeader from "./";
+
+import interstellarPoster from "./";
+import interstellarHeader from "./";
+
+import edgeOfSeventeenPoster from "./";
+import edgeOfSeventeenHeader from "./";
+
+export const films = {
+    "the-witch": {
+        title: "The Witch",
+        year: 2015,
+        director: "Robert Eggers",
+        posterSrc: "",
+        headerSrc: "",
+        tagline: "Evil takes many forms.",
+        synopsis: "In 1630s New England, William and Katherine lead a devout Christian life with five children, homesteading on the edge of an impassable wilderness, exiled from their settlement when William defies the local church. When their newborn son vanishes and crops mysteriously fail, the family turns on one another.",
+        cast: [],
+        crew: []
+    },
+    "little-women": {
+        title: "Little Women",
+        year: 2019,
+        director: "Greta Gerwig",
+        posterSrc: "",
+        headerSrc: "",
+        tagline: "Own your story",
+        synopsis: "Four sisters come of age in America in the aftermath of the Civil War",
+        cast: [],
+        crew: []
+    },
+    "lady-bird": {
+        title: "Lady Bird",
+        year: 2017,
+        director: "Greta Gerwig",
+        posterSrc: "",
+        headerSrc: "",
+        tagline: "Fly away home.",
+        synopsis: "A California high school student plans to escape from her family and small town by going to college in New York, much to the disapproval of wildly loving, deeply opinionated and strong-willed mother.",
+        cast: [],
+        crew: []
+    },
+    "knives-out": {
+        title: "Knives Out",
+        year: 2019,
+        director: "Rian Johnson",
+        posterSrc: "",
+        headerSrc: "",
+        tagline: "Hell, any of them could have done it.",
+        synopsis: "When renowned crime novelist Harlan Thrombey is found dead at his estate just after his 85th birthday, the inquisitive and debonair Detective Benoit Blanc is mysteriously enlisted to investigate. From Harlan's dysfunctional family to his devoted staff, Blanc sifts through a web of red herrings and self-serving lies to uncover the truth behind Harlan's untimely death.",
+        cast: [],
+        crew: []
+    },
+    "booksmart": {
+        title: "Booksmart",
+        year: 2019,
+        director: "Olivia Wilde",
+        posterSrc: "",
+        headerSrc: "",
+        tagline: "Getting straight A's. Giving zero F's.",
+        synopsis: "Two academic teenage superstars realize, on the eve of their high school graduation, that they should have worked less and played more. Determined to never fall short of their peers, the girls set out on a mission to cram four years of fun into one night.",
+        cast: [],
+        crew: []
+    },
+    "columbus": {
+        title: "Columbus",
+        year: 2017,
+        director: "Kogonada",
+        posterSrc: "",
+        headerSrc: "",
+        tagline: "In the middle of all the mess. . . there was this.",
+        synopsis: "When a renowned architecture scholar falls suddenly ill during a speaking tour, his son Jin finds himself stranded in Columbus, Indiana - a small Midwestern city celebrated for its many significant modernist buildings. Jin strikes up a friendship with Casey, a young architecture enthusiast who works at the local library.",
+        cast: [],
+        crew: []
+    },
+    "arrival": {
+        title: "Arrival",
+        year: 2016,
+        director: "Denis Villeneuve",
+        posterSrc: "",
+        headerSrc: "",
+        tagline: "Why are they here?",
+        synopsis: "Taking place after alien crafts land around the world, an expert linguist is recruited by the military to determine whether they come in peace or are a threat.",
+        cast: [],
+        crew: []
+    },
+    "star-wars-the-last-jedi": {
+        title: "Star Wars: The Last Jedi",
+        year: 2017,
+        director: "Rian Johnson",
+        posterSrc: "",
+        headerSrc: "",
+        tagline: "Darkness rises. . . and light to meet it.",
+        synopsis: "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.",
+        cast: [],
+        crew: []
+    },
+    "interstellar": {
+        title: "Interstellar",
+        year: 2014,
+        director: "Christopher Nolan",
+        posterSrc: "",
+        headerSrc: "",
+        tagline: "Mankind was born on Earth. It was never meant to die here.",
+        synopsis: "Interstellar chronicles the adventures of a group of explorers who make use of a newly discovered wormhole to surpass the limitations on human space travel and conquer the vast distances involved in an interstellar voyage.",
+        cast: [],
+        crew: []
+    },
+    "the-edge-of-seventeen": {
+        title: "The Edge of Seventeen",
+        year: 2016,
+        director: "Kelly Fremon Craig",
+        posterSrc: "",
+        headerSrc: "",
+        tagline: "You're only young once. . . is it over yet?",
+        synopsis: "Two high school girls are best friends until one dates the other's older brother, who is totally his sister's nemesis.",
+        cast: [],
+        crew: []
+    }
+};
