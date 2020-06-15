41265e01d129eae4c42635ec292cb4f2f47f3d2d
Kelly Fleming <kflemin3@ncsu.edu>
2020-06-15 15:37:11 -0400

    add function to get random path for random button in dropdown

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import {Switch, Route, Link} from "react-router-dom";
+import React, {useState, useCallback, useEffect} from 'react';
+import {Switch, Route, Link, useLocation} from "react-router-dom";
+import {getRandom} from "./utility";
 import Dropdown from "./Dropdown";
 import filmData from "./data";
 import './App.css';
 
 function App() {
+  const keys = Object.keys(filmData);
+  let [randomPath, setRandomPath] = useState(""); // change initial state later?
+  let location = useLocation();
+
+  const setRandomLink = useCallback(
+    (path) => {
+      let option = path.split("/")[1]; // either "" or key
+      let key = getRandom(keys);
+      if (option !== "") {
+        while (key === option) {
+          key = getRandom(keys);
+        }
+        setRandomPath(filmData[key].path);
+      } else {
+        setRandomPath(filmData[key].path);
+      }
+    }, [keys]
+  );
+
+  useEffect(() => {
+    setRandomLink(location.pathname);
+  }, [location.pathname, setRandomLink]);
+
   return (
     <div className="App">
         <Link to="/">Home</Link>
-        <Dropdown keys={Object.keys(filmData)} filmData={filmData}/>
+        <Dropdown keys={keys} randomPath={randomPath} filmData={filmData}/>
         <Switch>
             <Route>
                 {/* Film component */}
