7ad5ba8093412cc5f47d8d358218c5c96d9c8d50
Kelly Fleming <kflemin3@ncsu.edu>
2020-06-15 18:15:19 -0400

    add simple About page component

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+    return (
+        <div>
+            This single-page application emulates some of the functionality of Letterboxd.
+        </div>
+    );
+}
+
+export default About;
