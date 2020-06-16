66ba256dfdbdf605b04e978b898000ecd82f47a0
Kelly Fleming <kflemin3@ncsu.edu>
2020-06-16 17:43:36 -0400

    add HeaderImage component and basic markup

---
+++ src/HeaderImage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function HeaderImage() {
+    return (
+        <div>
+            <div>
+                <div>
+
+                </div>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    );
+}
+
+export default HeaderImage;
