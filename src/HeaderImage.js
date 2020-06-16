aa7c473854eef902eac3ff4fc1985930f67881c4
Kelly Fleming <kflemin3@ncsu.edu>
2020-06-16 18:07:37 -0400

    add HeaderImage component

--- src/HeaderImage.js
+++ src/HeaderImage.js
@@ -1,16 +1,21 @@
 import React from "react";
+import filmData from "./data";
+import {useLocation} from "react-router-dom";
+import "./HeaderImage.css";
 
 function HeaderImage() {
+    let location = useLocation();
+    let key = location.pathname.split("/")[1];
+    let headerSrc = "";
+    if (key) {
+        headerSrc = filmData[key].headerSrc;
+    }
     return (
-        <div>
-            <div>
-                <div>
-
-                </div>
+        <div className="Header-container">
+            <div id="header">
+                {headerSrc && <div className="Header-image" style={{backgroundImage: `url(${headerSrc})`}}></div>}
             </div>
-            <div>
-                
-            </div>
+            <div className="Header-mask"></div>
         </div>
     );
 }
