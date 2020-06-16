c6c0eb9519287bc63680d1f29a1e9c8bf64ec506
Kelly Fleming <kflemin3@ncsu.edu>
2020-06-16 18:36:22 -0400

    render Navbar component and refactor related prop passing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, {useState, useCallback, useEffect} from 'react';
-import {Switch, Route, Link, useLocation} from "react-router-dom";
+import {Switch, Route, useLocation} from "react-router-dom";
 import {getRandom} from "./utility";
 import HeaderImage from "./HeaderImage";
-import Dropdown from "./Dropdown";
+import Navbar from "./Navbar";
 import About from "./About";
 import Film from "./Film";
 import filmData from "./data";
-import './App.css';
 
 function App() {
   const keys = Object.keys(filmData);
@@ -34,14 +33,13 @@ function App() {
 
   return (
     <div className="App">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
         {/* header image and gradient mask */}
         <HeaderImage />
+        <Navbar randomPath={randomPath}/>
         <header>
           <div>
             {/* this is the gradient mask that sits behind the navbar and on the top of the header image */}
-            <Dropdown keys={keys} randomPath={randomPath} filmData={filmData}/>
+            {/* <Dropdown keys={keys} randomPath={randomPath} filmData={filmData}/> */}
           </div>
         </header>
         <Switch>
