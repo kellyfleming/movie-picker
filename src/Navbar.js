d39b4254e8f8e3660198b03dbc3b6f594e0ddf0a
Kelly Fleming <kflemin3@ncsu.edu>
2020-06-22 15:12:25 -0400

    remove mask from navbar if no header image is being displayed

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,24 @@
 import React from "react";
+import {useLocation} from "react-router-dom";
 import "./Navbar.css";
 import Dropdown from "./Dropdown";
 import filmData from "./data";
 
 function Navbar(props) {
+    let location = useLocation();
+    let path = location.pathname.split("/")[1];
+    let needGradient = path !== "about" && path !== "";
     return (
         <header className="Navbar-wrapper">
-            <div className="Navbar-background">
-                <div className="Navbar-container">
-                    <Dropdown keys={Object.keys(filmData)} randomPath={props.randomPath} filmData={filmData}/>
+            {needGradient
+                ? <div className="Navbar-background">
+                    <div className="Navbar-container">
+                        <Dropdown keys={Object.keys(filmData)} randomPath={props.randomPath} filmData={filmData} />
+                    </div>
                 </div>
-            </div>
+                : <div className="Navbar-container">
+                    <Dropdown keys={Object.keys(filmData)} randomPath={props.randomPath} filmData={filmData} />
+                </div>}
         </header>
     );
 }
