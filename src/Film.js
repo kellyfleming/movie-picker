d1b4bc92a04dcfb14f7b43d70874dd83d7b38e84
Kelly Fleming <kflemin3@ncsu.edu>
2020-06-22 15:55:10 -0400

    finish Film component markup and add class names

--- src/Film.js
+++ src/Film.js
@@ -8,28 +8,41 @@ function Film(props) {
     let key = params.film;
     let {title, year, director, posterSrc, synopsis, tagline} = filmData[key];
     return (
-        <main>
-            {/* wrapper */}
-            <div>
-                {/* container */}
-                <div>
-                    {/* sidebar stuff */}
-                    <div style={{backgroundImage: `url(${posterSrc})`, height: "300px", width: "200px"}}>
+        <main className="Main-wrapper">
+            <div className="Main-container">
+                <div className="Main-sidebar">
+                    <div className="Sidebar-poster" style={{backgroundImage: `url(${posterSrc})`, height: "300px", width: "200px"}}>
                     </div>
                 </div>
-                <div>
-                    <div>
+                <div className="Main-content">
+                    <div className="Content-title">
                         {title}
                         {year}
                         Directed by {director}
                     </div>
-                    <div>
-                        <div>
-                        {tagline}
-                        {synopsis}
-                        </div>
-                        <div>
+                    <div className="Content-wrapper">
+                        <div className="Content-left">
+                            <div className="Content-description">
+                                <p className="Description-tagline">
+                                    {tagline}
+                                </p>
+                                <p className="Description-synopsis">
+                                    {synopsis}
+                                </p>
+                            </div>
+                            <div className="Content-tabs">
 
+                            </div>
+                        </div>
+                        <div className="Content-right">
+                            <div className="Content-interaction-wrapper">
+                                <div className="Content-interaction-toggles">
+
+                                </div>
+                                <div className="Content-interaction-rating">
+                                    
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
