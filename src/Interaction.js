322a069de624fca1582bda9032bd674783eb5ed5
Kelly Fleming <kflemin3@ncsu.edu>
2020-06-24 16:50:30 -0400

    create Interaction component

---
+++ src/Interaction.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Interaction.css";
+import useLocalStorageToggle from "./hooks/useLocalStorageToggle";
+import useLocalStorageState from "./hooks/useLocalStorageState";
+
+function Interaction(props) {
+    let {likedKey, watchedKey, ratingKey} = props.keys;
+    let [isLiked, toggleisLiked] = useLocalStorageToggle(likedKey, false);
+    let [hasSeen, toggleHasSeen] = useLocalStorageToggle(watchedKey, false);
+    let [rating, setRating] = useLocalStorageState(ratingKey, 0);
+    let likeToggleClasses = `${isLiked ? "liked fas" : "unliked far"} fa-heart`;
+    let seenToggleClasses = `${hasSeen ? "seen fas" : "notSeen far"} fa-eye`;
+    return (
+        <div className="Interaction-wrapper">
+            <div className="Interaction-toggles">
+                <div className="Interaction-icon" onClick={toggleHasSeen}>
+                    <span id="eye" className={seenToggleClasses}></span>
+                    <span className="labelText">{hasSeen ? "Seen" : "Seen?"}</span>
+                </div>
+                <div className="Interaction-icon" onClick={toggleisLiked}>
+                    <span id="heart" className={likeToggleClasses}></span>
+                    <span className="labelText">{isLiked ? "Liked" : "Like?"}</span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Interaction;
