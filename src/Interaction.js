dfc40c82a756a93c5f077c9eed82c5ee64848d79
Kelly Fleming <kflemin3@ncsu.edu>
2020-07-10 11:46:04 -0400

    connect local storage to interaction toggles and components

--- src/Interaction.js
+++ src/Interaction.js
@@ -1,30 +1,62 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import StarRating from "./StarRating";
 import "./Interaction.css";
-import useLocalStorageToggle from "./hooks/useLocalStorageToggle";
-import useLocalStorageState from "./hooks/useLocalStorageState";
+import {getLocal, setLocal} from "./utility";
 
 function Interaction(props) {
     let {likedKey, watchedKey, ratingKey} = props.keys;
-    let [isLiked, toggleisLiked] = useLocalStorageToggle(likedKey, false);
-    let [hasSeen, toggleHasSeen] = useLocalStorageToggle(watchedKey, false);
-    let [rating, setRating] = useLocalStorageState(ratingKey, 0);
+
+    if (!getLocal(likedKey)) {
+        setLocal(likedKey, false);
+    }
+
+    if (!getLocal(watchedKey)) {
+        setLocal(watchedKey, false);
+    }
+
+    if (!getLocal(ratingKey)) {
+        setLocal(ratingKey, 0);
+    }
+    
+    let [isLiked, setIsLiked] = useState(JSON.parse(getLocal(likedKey)));
+    let [hasSeen, setHasSeen] = useState(JSON.parse(getLocal(watchedKey)));
+    let [rating, setRating] = useState(JSON.parse(getLocal(ratingKey)));
+
+    useEffect(() => {
+        setIsLiked(JSON.parse(getLocal(likedKey)));
+        setHasSeen(JSON.parse(getLocal(watchedKey)));
+        setRating(JSON.parse(getLocal(ratingKey)));
+    }, [likedKey, watchedKey, ratingKey]);
+
     let likeToggleClasses = `${isLiked ? "liked fas" : "unliked far"} fa-heart`;
     let seenToggleClasses = `${hasSeen ? "seen fas" : "notSeen far"} fa-eye`;
+
+    function toggle(key, setter) {
+        let current = JSON.parse(getLocal(key));
+        if (current !== null) {
+            setLocal(key, !current);
+            setter(!current);
+        }
+    }
+
+    function setSavedRating(val) {
+        setLocal(ratingKey, val);
+        setRating(val);
+    }
 
     return (
         <div className="Interaction-wrapper">
             <div className="Interaction-toggles">
-                <div className="Interaction-icon" onClick={toggleHasSeen}>
+                <div className="Interaction-icon" onClick={() => toggle(watchedKey, setHasSeen)}>
                     <span id="eye" className={seenToggleClasses}></span>
-                    <span className="labelText">{hasSeen ? "Seen" : "Seen?"}</span>
+                    <span className="labelText">{JSON.parse(getLocal(watchedKey)) ? "Seen" : "Seen?"}</span>
                 </div>
-                <div className="Interaction-icon" onClick={toggleisLiked}>
+                <div className="Interaction-icon" onClick={() => toggle(likedKey, setIsLiked)}>
                     <span id="heart" className={likeToggleClasses}></span>
-                    <span className="labelText">{isLiked ? "Liked" : "Like?"}</span>
+                    <span className="labelText">{JSON.parse(getLocal(likedKey)) ? "Liked" : "Like?"}</span>
                 </div>
             </div>
-            <StarRating savedValue={rating} setRating={setRating}/>
+            <StarRating savedValue={JSON.parse(getLocal(ratingKey))} setSavedRating={setSavedRating} />
         </div>
     );
 }
