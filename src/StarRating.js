699d2e1784454cca4c7a038191e444444be648e2
Kelly Fleming <kflemin3@ncsu.edu>
2020-07-10 11:48:07 -0400

    appropriately update star display on change

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,31 +1,39 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useCallback} from "react";
 import Star from "./Star";
 import useToggle from "./hooks/useToggle";
 
 function StarRating(props) {
     let numStars = 5;
+
+    const generateStars = useCallback(
+        (val) => {
+            let half = val % 1;
+            let whole = val - half;
+            let stars = Array(numStars).fill(0);
+            let i = 0;
+
+            while (whole !== 0) {
+                stars[i] = 1;
+                whole--;
+                i++;
+            }
+            if (i < 5) {
+                stars[i] = half;
+            }
+            return stars;
+        }, [numStars]
+    );
+
+    useEffect(() => {
+        setSavedStars(generateStars(props.savedValue));
+    }, [props.savedValue, generateStars]);
+
     let [savedStars, setSavedStars] = useState(generateStars(props.savedValue));
     let [hoverStars, setHoverStars] = useState(Array(numStars).fill(0));
     let [isHovering, toggleIsHovering] = useToggle(false);
 
-    function generateStars(val) {
-        let half = val % 1;
-        let whole = val - half;
-        let stars = Array(numStars).fill(0);
-        let i = 0;
-
-        while (whole !== 0) {
-            stars[i] = 1;
-            whole--;
-            i++;
-        }
-        if (i < 5) {
-            stars[i] = half;
-        }
-        return stars;
-    }
-
     function updateSavedStars(starValue, starIndex) {
+        console.log(starValue, starIndex);
         let updatedStars = savedStars.map((val, index) => {
             if (index < starIndex) {
                 return 1;
@@ -37,7 +45,7 @@ function StarRating(props) {
         });
         let total = updatedStars.reduce((t, star) => t + star, 0);
         setSavedStars(updatedStars);
-        props.setRating(total);
+        props.setSavedRating(total);
     }
 
     function generateSVGArray(arr) {
@@ -54,7 +62,7 @@ function StarRating(props) {
     return (
         <div>
             <span>
-                <span></span>
+                <span className="fas fa-ban"></span>
             </span>
             <div>
                 {generateSVGArray(savedStars)}
