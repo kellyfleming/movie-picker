e4fdcefdfd8e31629bd946731ea2af12e5eca678
Kelly Fleming <kflemin3@ncsu.edu>
2020-07-13 10:39:48 -0400

    style according to hover and add mouse over event

--- src/StarHalf.js
+++ src/StarHalf.js
@@ -4,15 +4,26 @@ import {ReactComponent as Outline} from "./outline.svg";
 import "./StarHalf.css";
 
 function StarHalf(props) {
+    let classes = `
+        ${props.isFlipped && "StarHalf-flipped"}
+        ${props.isHovering ? "StarHalf-hover" : "StarHalf-saved"}
+    `;
+
+    function handleMouseOver() {
+        let val = props.halfIndex === 0 ? 0.5 : 1;
+        props.updateHoverStars(val, props.parentIndex);
+    }
+    
     function handleClick() {
         let val = props.halfIndex === 0 ? 0.5 : 1;
         props.updateSavedStars(val, props.parentIndex);
     }
 
-    let classes = props.isFlipped ? "StarHalf-flipped" : "";
     return (
         <span className="StarHalf">
-            {props.isFilled ? <Solid className={classes} onClick={handleClick}/> : <Outline className={classes} onClick={handleClick}/>}
+            {props.isFilled 
+            ? <Solid className={classes} onClick={handleClick} onMouseOver={handleMouseOver}/> 
+            : <Outline className={classes} onClick={handleClick} onMouseOver={handleMouseOver}/>}
         </span>
     )
 }
