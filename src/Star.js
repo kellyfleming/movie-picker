9c7e96e8583ba3a70d5459ec2f51d054af1efb88
Kelly Fleming <kflemin3@ncsu.edu>
2020-07-13 10:38:12 -0400

    pass new props down to individual halves

--- src/Star.js
+++ src/Star.js
@@ -7,15 +7,19 @@ function Star(props) {
                 isFilled={props.value > 0}
                 isFlipped={false}
                 updateSavedStars={props.updateSavedStars}
+                updateHoverStars={props.updateHoverStars}
                 halfIndex={0}
                 parentIndex={props.pIndex}
+                isHovering={props.isHovering}
             />
             <StarHalf
                 isFilled={props.value === 1}
                 isFlipped={true}
+                updateHoverStars={props.updateHoverStars}
                 updateSavedStars={props.updateSavedStars}
                 halfIndex={1}
                 parentIndex={props.pIndex}
+                isHovering={props.isHovering}
             />
         </span>
     );
