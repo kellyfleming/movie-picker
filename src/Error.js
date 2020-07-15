7f67f8f4b88a527817b5058db17f1cf20ab93f53
Kelly Fleming <kflemin3@ncsu.edu>
2020-07-15 10:43:51 -0400

    create simple error component

---
+++ src/Error.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Error.css";
+
+function Error() {
+    return (
+        <div className="Error">
+            <h1>Oops, sorry, couldn't find that.</h1>
+            <p>Select a film from the dropdown above or click random for a surprise!</p>
+        </div>
+    );
+}
+
+export default Error;
