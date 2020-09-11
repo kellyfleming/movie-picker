6fe47147d586f1b65f8929bd1324d2e2638c9fc1
Kelly Fleming <kflemin3@ncsu.edu>
2020-09-11 12:45:15 -0400

    make error component responsive - resolves #3

--- src/Error.js
+++ src/Error.js
@@ -4,7 +4,8 @@ import "./Error.css";
 function Error() {
     return (
         <div className="error flex-col">
-            <h1>Oops, sorry, couldn't find that.</h1>
+            <h1>Oops, sorry!<br/>Couldn't find that.
+            </h1>
             <p>Select a film from the dropdown above or click random for a surprise!</p>
         </div>
     );
